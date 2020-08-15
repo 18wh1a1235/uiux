b46f4cc3e515411fecb4bbd6f76c236acf209615
amitjha25in <amitjha@3921>
2020-08-15 19:13:22 +0530

    ajax product load

--- data/data.php
+++ data/data.php
@@ -1,29 +1,101 @@
 <?php
-
-if ($_POST["dataId"]) {
-   $dataId=  $_POST["dataId"];
-  $html_data = '';
-   if($dataId == 1){
-        $html_data .='<h1>One</h1>';
-        echo  $html_data;
-   }
-   if($dataId == 2){
-    $html_data .='
-                <h1>Two</h1>       
-        
-        ';
-        echo  $html_data;
-   }
-   if($dataId == 3){
-    $html_data .='
-            <h1>Three</h1>       
-
-        ';
-        echo  $html_data;
-   }
-   
-} 
-
-
+     if($_POST["dataId"]){
+          $postData = $_POST["dataId"];
+          $output = '';
+          if($postData == "bestSellers"){
+               $output .='
+               <div class="product_data" id="bestSellers">
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/ring.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/chain.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/earring.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/earring_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/earring_3.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/mangalsutra.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/ring_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/ring_3.jpg">
+               </div>
+           </div>
+               
+               ';
+               echo  $output;
+          }
+          if($postData == "newArrival"){
+               $output .='
+               <div class="product_data" id="newArrival">
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/band.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/bange.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/bangle.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/earring.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/earring_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/necklace.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/necklace_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/new_arrivals/ring.jpg">
+               </div>
+           </div>
+               ';
+               echo  $output;
+          }
+          if($postData == "recentViewed"){
+               $output .='
+               <div class="product_data" id="recentViewed">
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/bracelet.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/earring.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/earring_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/kada.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/pendant.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/pendant_2.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/recent_viewed/ring.jpg">
+               </div>
+               <div class="item" data-aos="zoom-out">
+                   <img src="images/bestSellers/ring_2.jpg">
+               </div>
+           </div>
+               ';  
+               echo  $output;
+          }
+     }
 
 ?>
