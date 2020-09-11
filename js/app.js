a921dcef91573067793cf7ee6d98ffc7cfd5f8a1
amitjha25in <amitjha@3921>
2020-09-11 15:28:26 +0530

    user search

--- js/app.js
+++ js/app.js
@@ -1,62 +1,142 @@
-//https://randomuser.me/api/?gender=female
+//https://api.github.com/users/user_name/repos
+//
 
-//reload page
-const refresh = document.querySelector(".refresh");
-refresh.addEventListener("click", function () {
-    location.reload();
+const user_img = document.querySelector(".user_img");
+const userName = document.querySelector(".user_name h1");
+const followers_ = document.querySelector(".followers_ span");
+const follow_ = document.querySelector(".follow_ span");
+const repo_details = document.querySelector(".repo_details");
+const btn_submit = document.querySelector(".btn_submit");
+
+
+let user_name = '';
+
+//when user writer user name in text box
+function inputFunction() {
+    let input_user = document.querySelector(".input_user").value.trim();
+    //trim method will replace before and after white space of given calue
+
+    if (input_user.length <= 0) {
+        alert("Please enter github user name");
+        document.querySelector(".input_user").value = "";
+        document.querySelector(".input_user").focus();
+        return false;
+    } else {
+        user_name = input_user.split("").join("");
+        //if everything is ok run fetch user funciton
+        fetchUser(); // this funciton is not made yet
+
+        //clear the input box and focused it for next
+        document.querySelector(".input_user").value = "";
+        document.querySelector(".input_user").focus();
+    }
+};
+
+btn_submit.addEventListener("click", function () {
+    inputFunction()
+});
+
+// if user press enter it should be submit 
+document.querySelector(".input_user").addEventListener("keyup", function (e) {
+    if (e.keyCode === 13) {
+        //alert("you have pressed enter key");
+        inputFunction()
+    }
 })
 
+//fetching user from github api
+function fetchUser() {
+    fetch(`https://api.github.com/users/${user_name}`)
+        .then(response => response.json())
+        .then(function (data) {
+            //I not testing live because unregistered user can hit data only 60 time per hour
+            console.log(data);
+            if (data.message === "Not Found") {
+                alert("user not found");
+                return false;
+            } else {
+                user_img.innerHTML = `<img src="${data.avatar_url}">`;
+                userName.innerHTML = data.login;
+                followers_.innerHTML = data.followers;
+                follow_.innerHTML = data.following;
 
-const title_details_ = document.querySelector(".title_details");
-const info_icons = document.querySelector(".info_icons");
-const user_img = document.querySelector(".user_img");
-
-//fetching api data
-fetch('https://randomuser.me/api/?gender=female')
-    .then(response => response.json())
-    .then(function (data) {
-        //console.log(data);
-        let userData = data.results[0];
-        console.log(userData);
-        let imgSrc = `<img src="${userData.picture.large}">`;
-        let name_ = `${userData.name.first} ${userData.name.last}`;
-        let content_ = `
-        <div class="icon user_ active" data-title="Hi, My name is" data-value="${userData.name.first} ${userData.name.last}"></div>
-        <div class="icon email_" data-title="My email address is" data-value="${userData.email}"></div>
-        <div class="icon birthday_" data-title="My age is" data-value="${userData.dob.age}yrs"></div>
-        <div class="icon location_" data-title="My addres is" data-value="${userData.location.country}"></div>
-        <div class="icon phone_" data-title="My phone number is " data-value="${userData.cell}"></div>
-        <div class="icon password_" data-title="My password is" data-value="${userData.login.password}"></div>        
-        `
-        info_icons.innerHTML = content_;
-        user_img.innerHTML = imgSrc;
-        title_details_.innerHTML = name_;
-
-
-        //for hover and change details
-        const icon = document.querySelectorAll(".icon");
-        const title_info = document.querySelector(".title_info");
-        const title_details = document.querySelector(".title_details");
-
-        icon.forEach(function (item) {
-            item.addEventListener("mouseover", function () {
-                let titleData = item.getAttribute("data-title");
-                let dataValue = item.getAttribute("data-value");
-
-                title_info.innerHTML = titleData;
-                title_details.innerHTML = dataValue;
-
-                let activeClass = document.querySelectorAll(".active");
-
-                activeClass.forEach(function (active_) {
-                    active_.className = active_.className.replace(" active", "");
-                })
-                item.className += " active";
-
-            });
+            }
         })
 
+    //fetching repo
+    fetch(`https://api.github.com/users/${user_name}/repos`)
+        .then(response => response.json())
+        .then(function (repo_data) {
+            console.log(repo_data);
+            //if user type random name which is user but not have repository
+            if (repo_data.length <= 0) {
+                repo_details.innerHTML = `
+                
+                <div class="item_">
+                    <div class="repo_name">No Repo Found</div>                
+                </div>
+                
+                `
+            } else {
+                //when you type random user name if user and repo both not found
+                if (repo_data.message === "Not Found") {
+                    repo_details.innerHTML = `
+                    <div class="item_">
+                        <div class="repo_name">devAmit</div>
+                        <div class="repo_details_">
+                            <div class="info_ star">
+                                <i class="fa fa-star-o"></i>10
+                            </div>
+                            <div class="info_ fork">
+                                <p><i class="fa fa-code-fork"></i>30</p>
+                            </div>
+                            <div class="info_ size">
+                                <p><i class="fa fa-file"></i>3000kb</p>
+                            </div>
+                        </div>
+                    </div>                
+                    `
+                    user_img.innerHTML = `<img src="images/github_logo.png">`;
+                    userName.innerHTML = `devAmit`;
+                    followers_.innerHTML = "500";
+                    follow_.innerHTML = "50";
+                } else {
+                    let repo_Data = repo_data.map(item => {
+                        console.log(item);
+                        return (
+                            `
+                            <div class="item_">
+                                <div class="repo_name">${item.name}</div>
+                                <div class="repo_details_">
+                                    <div class="info_ star">
+                                        <i class="fa fa-star-o"></i>
+                                        ${item.watchers}
+                                    </div>
+                                    <div class="info_ fork">
+                                        <p><i class="fa fa-code-fork"></i>
+                                        ${item.forks}
+                                        </p>
+                                    </div>
+                                    <div class="info_ size">
+                                        <p><i class="fa fa-file"></i>
+                                        ${item.size}kb
+                                        </p>
+                                    </div>
+                                </div>
+                            </div> 
+                            `
+                        );
+                    })
+                    //I am taking maximum 6 repos
+                    // you can tak according to your requirement
+                    repo_details.innerHTML = repo_Data.slice(0, 6).join("");
 
-    })
-//please subscribe my channel for this type of javascript tutorials
-//thank you
+                }
+            }
+
+        });
+}
+
+//thank for watching
+//please subscribe my channel for this type of turorials
+// keep watching
